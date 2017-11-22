9a4e5e63c24f3656187b537d2f2d783fa4376d61
Mo Li <mo.vico.li@gmail.com>
2017-11-22 16:05:25 -0600

    Delete NEI_point.py

--- NEI_point.py
+++
@@ -1,7 +0,0 @@
-#NEI import and processing
-#This script uses the NEI Point National Data File.
-#Data files:ftp://ftp.epa.gov/EmisInventory/2014/2014neiv1_facility_process_byregions.zip
-
-import pandas as pd
-import os
-
