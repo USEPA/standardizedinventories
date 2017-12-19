900379c022dea6aa6e99d69095038dd68842dfef
Mo Li <mo.vico.li@gmail.com>
2017-12-19 13:09:13 -0600

    modified NEI.py to generate standardized NEI table

--- NEI.py
+++ NEI.py
@@ -6,7 +6,7 @@ import pandas as pd
 nei_required_fields = pd.read_table('./data/NEI_required_fields.csv',sep=',').fillna('Null')
 nei_file_path = pd.read_table('./data/NEI_file_path.csv',sep=',').fillna('Null')
 
-def read_data(file):
+def read_data(source,file):
     tmp = pd.Series(list(nei_required_fields[source]), index=list(nei_required_fields['StandardizedEPA']))
     file_result = pd.DataFrame(columns=[x for x in tmp[tmp!='Null'].index if x!='Amount']+['sum'])
     # read nei file by chunks
@@ -24,7 +24,7 @@ def standardize_output(source): # source as 'Point'/'NonPoint'/'OnRoad'/'NonRoad
     file_path = list(set(nei_file_path[source]) - set(['Null']))
 
     # read in first nei file by chunks
-    nei = read_data(file_path[0])
+    nei = read_data(source,file_path[0])
     print(len(nei))
     # read in other nei files and concatenate all nei files into one dataframe
     for file in file_path[1:]:
@@ -34,7 +34,21 @@ def standardize_output(source): # source as 'Point'/'NonPoint'/'OnRoad'/'NonRoad
         nei = nei.groupby(nei.columns[:-1].tolist())['sum'].agg(['sum']).reset_index()
         print(file)
         print(len(nei))
-    # change column name
-    nei.rename(columns={'sum':'Amount'},inplace=True)
+    # convert LB/TON to KG
+    nei['Amount'] = np.where(nei['UOM']=='LB',nei['sum']*0.453592,nei['sum']*907.184)
+    # drop UOM and sum column
+    nei = nei.drop(['UOM','sum'],1)
+    # add Source column
+    nei['Source'] = source
     return(nei)
 
+point = standardize_output('Point')
+nonpoint = standardize_output('NonPoint')
+onroad = standardize_output('OnRoad')
+nonroad = standardize_output('NonRoad')
+nei = pd.concat([point,nonpoint,onroad,nonroad],axis=0,ignore_index=True)
+nei = nei.fillna('')
+
+#Output to CSV
+outputdir='output/'
+nei.to_csv(outputdir+'NEI_2014.csv',index=False)
