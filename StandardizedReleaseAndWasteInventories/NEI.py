53e5a1499607acb165f13e1fa115b895bc98a0e2
WesIngwersen <ingwersen.wesley@epa.gov>
2018-05-02 23:33:18 -0400

    Major updates
    -Add facility aggregation
    -Update field names

--- StandardizedReleaseAndWasteInventories/NEI.py
+++ StandardizedReleaseAndWasteInventories/NEI.py
@@ -6,20 +6,22 @@ import pandas as pd
 import numpy as np
 import math
 
+report_year = '2011'
+
 output_dir = globals.output_dir
 data_dir = globals.data_dir
 nei_required_fields = pd.read_table(data_dir + 'NEI_required_fields.csv',sep=',').fillna('Null')
-nei_file_path = pd.read_table(data_dir + 'NEI_file_path.csv',sep=',').fillna('Null')
+nei_file_path = pd.read_table(data_dir + 'NEI_' + report_year + '_file_path.csv',sep=',').fillna('Null')
 
 def read_data(source,file):
     tmp = pd.Series(list(nei_required_fields[source]), index=list(nei_required_fields['StandardizedEPA']))
-    file_result = pd.DataFrame(columns=[x for x in tmp[tmp!='Null'].index if x!='Amount']+['sum'])
+    file_result = pd.DataFrame(columns=[x for x in tmp[tmp!='Null'].index if x!='FlowAmount']+['sum'])
     # read nei file by chunks
     for file_chunk in pd.read_table(file,sep=',',usecols=list(set(nei_required_fields[source])-set(['Null'])),chunksize=100000,engine='python'):
         # change column names to Standardized EPA names
         file_chunk = file_chunk.rename(columns=pd.Series(list(nei_required_fields['StandardizedEPA']),index=list(nei_required_fields[source])).to_dict())
         # aggregate
-        file_chunk = file_chunk.groupby([x for x in tmp[tmp!='Null'].index if x!='Amount'])['Amount'].agg(['sum']).reset_index()
+        file_chunk = file_chunk.groupby([x for x in tmp[tmp!='Null'].index if x!='FlowAmount'])['FlowAmount'].agg(['sum']).reset_index()
         # concatenate all chunks
         file_result = pd.concat([file_result,file_chunk])
     return file_result.groupby(file_result.columns[:-1].tolist())['sum'].agg(['sum']).reset_index()
@@ -40,7 +42,7 @@ def standardize_output(source): # source as 'Point'/'NonPoint'/'OnRoad'/'NonRoad
         print(file)
         print(len(nei))
     # convert LB/TON to KG
-    nei['Amount'] = np.where(nei['UOM']=='LB',nei['sum']*0.453592,nei['sum']*907.184)
+    nei['FlowAmount'] = np.where(nei['UOM']=='LB',nei['sum']*0.453592,nei['sum']*907.184)
     # add not included standardized columns as empty columns
     nei = pd.concat([nei,pd.DataFrame(columns=list(set(nei_required_fields['StandardizedEPA']) - set(nei.columns)))])
     nei = nei.fillna('')
@@ -61,16 +63,31 @@ def standardize_output(source): # source as 'Point'/'NonPoint'/'OnRoad'/'NonRoad
     nei = nei.drop(['UOM','sum'],1)
     return(nei)
 
+def nei_aggregate_unit_to_facility_level(nei_unit):
+    grouping_vars = ['FacilityID','NAICS','FlowName','State']
+    neibyfacility = nei_unit.groupby(grouping_vars)[['FlowAmount']]
+
+    neibyfacilityagg = neibyfacility.agg([('FlowAmount','sum')])
+    neibyfacilityagg = neibyfacility.agg(sum)
+
+    neibyfacilityaggref = neibyfacilityagg.reset_index()
+    return(neibyfacilityaggref)
+
 #NEIPoint
-point = standardize_output('Point')
-point_row = math.floor(len(point)/6)
+nei_unit = standardize_output('Point')
+nei_facility = nei_aggregate_unit_to_facility_level(nei_unit)
+
+#For use in NEI flowbyunitprocess
+#Needs revision
+#point_row = math.floor(len(point)/6)
 #break down NEIPoint into 6 parts
-point1 = point.iloc[:point_row,]
-point2 = point.iloc[point_row:point_row*2,]
-point3 = point.iloc[point_row*2:point_row*3,]
-point4 = point.iloc[point_row*3:point_row*4,]
-point5 = point.iloc[point_row*4:point_row*5,]
-point6 = point.iloc[point_row*5:,]
+#point1 = point.iloc[:point_row,]
+#point2 = point.iloc[point_row:point_row*2,]
+#point3 = point.iloc[point_row*2:point_row*3,]
+#point4 = point.iloc[point_row*3:point_row*4,]
+#point5 = point.iloc[point_row*4:point_row*5,]
+#point6 = point.iloc[point_row*5:,]
+
 #NEINonPoint
 nonpoint = standardize_output('NonPoint')
 #NEIOnRoad
@@ -79,12 +96,15 @@ onroad = standardize_output('OnRoad')
 nonroad = standardize_output('NonRoad')
 
 #Output to CSV
-point1.to_csv(output_dir + 'NEIPoint1_2014.csv', index=False)
-point2.to_csv(output_dir + 'NEIPoint2_2014.csv', index=False)
-point3.to_csv(output_dir + 'NEIPoint3_2014.csv', index=False)
-point4.to_csv(output_dir + 'NEIPoint4_2014.csv', index=False)
-point5.to_csv(output_dir + 'NEIPoint5_2014.csv', index=False)
-point6.to_csv(output_dir + 'NEIPoint6_2014.csv', index=False)
+#point1.to_csv(output_dir + 'NEIPoint1_2014.csv', index=False)
+#point2.to_csv(output_dir + 'NEIPoint2_2014.csv', index=False)
+#point3.to_csv(output_dir + 'NEIPoint3_2014.csv', index=False)
+#point4.to_csv(output_dir + 'NEIPoint4_2014.csv', index=False)
+#point5.to_csv(output_dir + 'NEIPoint5_2014.csv', index=False)
+#point6.to_csv(output_dir + 'NEIPoint6_2014.csv', index=False)
+
+nei_facility.to_csv(output_dir+'NEI_'+report_year+'.csv')
+
 nonpoint.to_csv(output_dir + 'NEINonPoint_2014.csv', index=False)
 onroad.to_csv(output_dir + 'NEIOnRoad_2014.csv', index=False)
 nonroad.to_csv(output_dir + 'NEINonRoad_2014.csv', index=False)
