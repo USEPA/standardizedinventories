a80eea385f39cfe2f66fcfc37d5f503d2a3b0225
WesIngwersen <ingwersen.wesley@epa.gov>
2018-05-07 12:34:01 -0400

    Change package name to stewi, update license and name

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     version="0.1",
     author="Wesley Ingwersen",
     author_email="ingwersen.wesley@epa.gov",
-    description="Standardized Release And Waste Inventories provides processed EPA release and emissions inventories "
+    description="Standardized Emission And Waste Inventories (StEWI) provides processed EPA release and emissions inventories "
                 "in standard tabular format",
-    license="GPL 3.0",
+    license="CC0",
     keywords="USEPA data",
-    url="http://www.github.com/usepa/lci-primer",
-    packages=['StandardizedReleaseAndWasteInventories'],
-    package_data={'StandardizedReleaseAndWasteInventories': ["data/*.*","output/*.*"]},
+    url="http://www.github.com/usepa/standardizedinventories",
+    packages=['stewi'],
+    package_data={'stewi': ["data/*.*","output/*.*"]},
     install_requires=['numpy', 'pandas', 'requests'],
     long_description=open('README.md').read(),
     classifiers=[
