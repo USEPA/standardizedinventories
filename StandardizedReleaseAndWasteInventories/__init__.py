2ed030f14457158754dcef6cd864e932ae14e283
WesIngwersen <ingwersen.wesley@epa.gov>
2018-05-07 11:05:35 -0400

    Specify cols and dtypes returned with getInventory

--- StandardizedReleaseAndWasteInventories/__init__.py
+++ StandardizedReleaseAndWasteInventories/__init__.py
@@ -2,21 +2,27 @@
 ## Currently just an initial outline
 
 import pandas as pd
+from StandardizedReleaseAndWasteInventories.globals import get_required_fields
 
-#reads a list of available inventories and prints them here, like:
-#National Emissions Inventory: 2014
-#Greenhouse Gas Reporting Program: 2015, 2016
-#....
+
 
 outputpath = 'StandardizedReleaseandWasteInventories/output/'
 formatpath = {'flowbyfacility':""}
 
 def seeAvailbleInventoriesandYears ():
+# reads a list of available inventories and prints them here, like:
+# National Emissions Inventory: 2014
+# Greenhouse Gas Reporting Program: 2015, 2016
+# ....
     print("Test")
 
 def getInventory(inventory_acronym,year,format='flowbyfacility',filter_for_LCI=False,US_States_Only=False):
+#Returns an inventory file as a data frame
     path = outputpath+formatpath[format]
     file = path+inventory_acronym+'_'+str(year)+'.csv'
-    inventory = pd.read_csv(file,header=0)
+    required_fields = get_required_fields(format)
+    cols = list(required_fields.keys())
+    inventory = pd.read_csv(file,header=0,usecols=cols,dtype=required_fields)
     return inventory
+
 
