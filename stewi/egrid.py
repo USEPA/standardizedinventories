e4b4bc4d5ae89962c3d2dfd4380da4dbee1bad40
ERG\TGhosh <Tapajyoti.Ghosh@erg.com>
2018-05-14 17:56:45 -0400

    Changes Ac to Wes
    Changed units and spellings. deleted unnecessary files

--- stewi/egrid.py
+++ stewi/egrid.py
@@ -11,11 +11,9 @@ import pandas as pd
 import os
 
 
-
-
-
 # Set some metadata
-eGRIDyear = '2014'
+eGRIDyear = '2016'
+#eGRIDyear = '2014'
 #output_dir = globals.output_dir #@author: Wes
 #data_dir = globals.data_dir #@author: Wes
 data_dir = os.path.dirname(os.path.realpath(__file__))
@@ -27,9 +25,11 @@ eGRIDfilepath = os.path.dirname(os.path.realpath(__file__))
 
 #filename for 2014
 #eGRIDfile = eGRIDfilepath + 'eGRID2014_Data_v2.xlsx' #@author: Wes
-eGRIDfile = eGRIDfilepath + '\\eGRID\\eGRID2014_Data_v2.xlsx'
+#eGRIDfile = eGRIDfilepath + '\\data\\eGRID2014_Data_v2.xlsx'
+eGRIDfile = eGRIDfilepath + '\\data\\eGRID2016.xlsx'
 
-pltsheetname = 'PLNT14'
+pltsheetname = 'PLNT16'
+#pltsheetname = 'PLNT14'
 
 # Import list of fields from egrid that are desired for LCI
 def imp_fields(fields_txt):
@@ -37,8 +37,8 @@ def imp_fields(fields_txt):
     egrid_req_fields = list(egrid_req_fields_df.values)
     return egrid_req_fields[0]
 
-#egrid_required_fields = (imp_fields(data_dir+'eGRID_required_fields.txt')) #@author: Wes
-egrid_required_fields = (imp_fields(data_dir+'\\eGRID_required_fields.txt'))
+egrid_required_fields = (imp_fields(data_dir+'\\data\\eGRID_required_fields.txt')) #@author: Wes
+#egrid_required_fields = (imp_fields(data_dir+'\\data\\eGRID_required_fields.txt'))
 
 # Import egrid file
 egrid = pd.read_excel(eGRIDfile, sheet_name=pltsheetname, skipinitialspace = True)
@@ -63,16 +63,14 @@ def unit_convert(value,factor):
 def createfacilityfile(): 
    
    
-    facility1=egrid2[['DOE/EIA ORIS plant or facility code','Plant state abbreviation']]
-   
+    facility1=egrid2[['DOE/EIA ORIS plant or facility code','Plant state abbreviation']]   
     facility2=egrid2[['DOE/EIA ORIS plant or facility code','Plant state abbreviation','eGRID subregion acronym','Plant county name','Plant latitude', 'Plant longitude','Plant primary fuel','Plant primary coal/oil/gas/ other fossil fuel category']]
-  
     os.chdir(data_dir)
     
-    facility1.rename(columns={'DOE/EIA ORIS plant or facility code':'FacilityID','Plant state abbreviation':'State'},inplace=True)
-    #facility1.to_csv('eGRID_2014.csv', index=False)
+    #facility1.rename(columns={'DOE/EIA ORIS plant or facility code':'FacilityID','Plant state abbreviation':'State'},inplace=True)    
     facility2.rename(columns={'DOE/EIA ORIS plant or facility code':'FacilityID','Plant state abbreviation':'State'},inplace=True)
-    facility2.to_csv('eGRID_2014.csv', index=False)
+    facility2.to_csv('eGRID_2016.csv', index=False)
+    #facility2.to_csv('eGRID_2014.csv', index=False)
 
 #Use this line for printing the column headers. Already done. 
 #names = egrid.columns.values
@@ -83,16 +81,16 @@ def createfacilityfile():
 
 #Need to change column names manually
 def createflowbyfacility():
-    flow = egrid2[['DOE/EIA ORIS plant or facility code','Plant total annual heat input (MMBtu)','Plant annual net generation (MWh)','Plant annual NOx total output emission rate (lb/MWh)','Plant annual SO2 total output emission rate (lb/MWh)','Plant annual CO2 total output emission rate (lb/MWh)','Plant annual CH4 total output emission rate (lb/GWh)','Plant annual N2O total output emission rate (lb/GWh)']]
-    flow.rename(columns={'DOE/EIA ORIS plant or facility code':'FacilityID','Plant total annual heat input (MMBtu)':'Plant total annual heat input (MJ)','Plant annual net generation (MWh)':'Plant annual net generation (MJ)', 'Plant annual NOx total output emission rate (lb/MWh)':'NOX (kg/MWh)','Plant annual SO2 total output emission rate (lb/MWh)':'SO2(kg/MWh)','Plant annual CO2 total output emission rate (lb/MWh)':'CO2(kg/MWh)','Plant annual CH4 total output emission rate (lb/GWh)':'CH4(kg/GWh)','Plant annual N2O total output emission rate (lb/GWh)':'N2O(kg/GWh)'},inplace=True)
-    
-    flow1 = unit_convert(flow[flow.columns[3:8]],0.4535924)
+    flow = egrid2[['DOE/EIA ORIS plant or facility code','Plant total annual heat input (MMBtu)','Plant annual net generation (MWh)', 'Plant annual NOx emissions (tons)','Plant annual SO2 emissions (tons)','Plant annual CO2 emissions (tons)','Plant annual CH4 emissions (lbs)','Plant annual N2O emissions (lbs)']]
+    flow.rename(columns={'DOE/EIA ORIS plant or facility code':'FacilityID','Plant total annual heat input (MMBtu)':'Heat input','Plant annual net generation (MWh)':'Net generation', 'Plant annual NOx emissions (tons)':'Nitrogen oxides','Plant annual SO2 emissions (tons)':'Sulfur dioxide','Plant annual CO2 emissions (tons)':'Carbon dioxide','Plant annual CH4 emissions (lbs)':'Methane','Plant annual N2O emissions (lbs)':'Nitrous oxide'},inplace=True)
+    flow1 = unit_convert(flow[flow.columns[3:6]],1000)
+    flow1_1 = unit_convert(flow[flow.columns[6:8]],0.4535924)
     flow2 = unit_convert(flow[flow.columns[1]],1055.056)
     flow3 = unit_convert(flow[flow.columns[2]],3600)
     flow4 = flow[['FacilityID']] 
-    frames = [flow4,flow2,flow3,flow1]
+    frames = [flow4,flow2,flow3,flow1,flow1_1]
     flow5 = pd.concat(frames,axis = 1)
-    flow6 = pd.melt(flow5, id_vars=['FacilityID','Plant total annual heat input (MJ)','Plant annual net generation (MJ)'], value_vars=list(flow5.columns[3:]), var_name='FlowName', value_name='FlowAmount')    
+    flow6 = pd.melt(flow5, id_vars=['FacilityID'], value_vars=list(flow5.columns[1:]), var_name='FlowName', value_name='FlowAmount')    
     return flow6;
 
 
@@ -104,8 +102,10 @@ flow7 = createflowbyfacility();
 flow7['ReliabilityScore'] = 0;
 #Dropping na emissions
 flowbyfac = flow7.dropna(subset=['FlowAmount'])
+flowbyfac = flowbyfac.sort_values(by = ['FacilityID'], axis=0, ascending=True, inplace=False, kind='quicksort', na_position='last')
 os.chdir(newpath)
-flowbyfac.to_csv('eGRID_2014.csv', index=False)
+#flowbyfac.to_csv('eGRID_2014.csv', index=False)
+flowbyfac.to_csv('eGRID_2016.csv', index=False)
 
 createfacilityfile()
 
