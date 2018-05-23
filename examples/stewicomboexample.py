24bee56cc5ac8b55f08c3d59f5f1b10a0decd869
WesIngwersen <ingwersen.wesley@epa.gov>
2018-05-23 15:30:21 -0400

    First stewicombo example

---
+++ examples/stewicomboexample.py
@@ -0,0 +1,9 @@
+import stewi
+import stewicombo
+
+stewi.seeAvailableInventoriesandYears()
+#Enter inventories that you would like to combine in the "Inventory_acryonym":"year" format enclosed in "{}"
+inventories_to_get = {"TRI":"2014","NEI":"2014"}
+
+inventories = stewicombo.combineInventories(inventories_to_get)
+
