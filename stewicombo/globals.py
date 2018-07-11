4b3ceb5a07fb1918ce7948322a6e2c751a7101ac
WesIngwersen <ingwersen.wesley@epa.gov>
2018-07-11 09:26:39 -0400

    Add inventory_preference_by_compartment

---
+++ stewicombo/globals.py
@@ -0,0 +1,5 @@
+#Variables and functions for common use
+inventory_preference_by_compartment = {"air":["eGRID","GHGRP","NEI","TRI"],
+                                       "water":["DMR", "TRI"],
+                                       "soil":["TRI"],
+                                       "waste":["RCRAInfo","TRI"]}
