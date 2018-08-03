6c8abb6cc8dfe3631ed2448ef071d5a6d108bd36
WesIngwersen <ingwersen.wesley@epa.gov>
2018-08-03 15:27:47 -0400

    Remove old function calls from facilitymatcher example

--- examples/getfacilitymatchesforEPAinventories.py
+++ examples/getfacilitymatchesforEPAinventories.py
@@ -1,15 +1,9 @@
 import facilitymatcher
 
+#Get facility matches for TRI
 facilitymatches = facilitymatcher.get_matches_for_inventories(["TRI"])
+#See the first 50
+facilitymatches.head(50)
 
-#Get matches only for list of facilities in one inventory
-from_inventory = "eGRID"
-to_inventories = ["TRI","NEI"]
-facilitymatchesforoneinventory = facilitymatcher.get_table_of_matches_from_program_to_programs_of_interest(from_inventory,to_inventories)
-#Peek at it
-facilitymatchesforoneinventory.head()
-
-#Count the matches
-facilitymatcher.count_matches_from_inventory_to_inventories_of_interest(from_inventory,to_inventories)
 
 
