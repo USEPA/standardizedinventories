f523029922c47bf067ca58daedd97b462201f9fb
WesIngwersen <ingwersen.wesley@epa.gov>
2019-09-11 13:42:38 -0400

    Specify data subdirectories to be included with pip install

--- setup.py
+++ setup.py
@@ -5,16 +5,24 @@ setup(
     version="0.9.1",
     author="Wesley Ingwersen",
     author_email="ingwersen.wesley@epa.gov",
-    description="Standardized Emission And Waste Inventories (StEWI) provides processed EPA release and emissions inventories "
+    description="Standardized Emission And Waste Inventories (StEWI)"
+                "provides processed EPA release and emissions inventories "
                 "in standard tabular format",
     license="CC0",
     keywords="USEPA data",
     url="http://www.github.com/usepa/standardizedinventories",
     packages=['chemicalmatcher','facilitymatcher','stewi','stewicombo'],
-    package_data={'stewi': ["data/*.*","output/*.*"],
+    #Must include package data, specifying all subdirectories to be included
+    #https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files
+    package_data={'stewi': ["data/*.*",
+                            "output/*.*",
+                            "output/facility/*.*"
+                            "output/flow/*.*"
+                            "output/flowbyfacility/*.*",
+                            "output/validation/*.*"],
                   'chemicalmatcher': ["data/*.*", "output/*.*"],
                   'facilitymatcher': ["data/*.*", "output/*.*"]},
-    include_package_data=True, #https://python-packaging.readthedocs.io/en/latest/non-code-files.html
+    include_package_data=True,
     install_requires=['numpy>=1.16','pandas>=0.22', 'requests>=2.20'],
     classifiers=[
         "Development Status :: Beta",
