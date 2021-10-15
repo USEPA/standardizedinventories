55e09c7c08ec41521ee66b57a552fa91db2440fe
Ben Young <Ben.Young@erg.com>
2021-10-15 09:36:56 -0400

    do not change path for testing file

--- tests/test_inventory_generation.py
+++ tests/test_inventory_generation.py
@@ -1,15 +1,12 @@
 """Test the creation of all inventories."""
 
 import stewi
-from stewi.globals import paths, STEWI_VERSION, config
+from stewi.globals import config
 
 year = 2018
 
 
 def test_inventory_generation():
-    # Create new local path
-    paths.local_path = paths.local_path + "_" + STEWI_VERSION
-
     error_list = []
     for inventory in config()['databases']:
         # skip RCRAInfo due to browswer download, DMR due to time constraints
