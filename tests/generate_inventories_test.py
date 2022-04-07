373c25518fdb7ff24260cc022396dac85f65eaf7
Ben Young <Ben.Young@erg.com>
2022-04-07 16:15:03 -0400

    rename to be discovered

--- tests/generate_inventories_test.py
+++ tests/generate_inventories_test.py
@@ -11,7 +11,7 @@ SKIP_BROWSER_DOWNLOAD = True
 
 
 @pytest.mark.inventory
-def generate_inventories(year):
+def generate_inventories_test(year):
     for inventory in config()['databases']:
         if SKIP_BROWSER_DOWNLOAD and inventory in requires_browser_download:
             continue
@@ -22,4 +22,4 @@ def generate_inventories(year):
 
 if __name__ == "__main__":
     year = 2017
-    generate_inventories(year)
+    generate_inventories_test(year)
