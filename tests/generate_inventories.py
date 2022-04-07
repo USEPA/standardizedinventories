9deee1441c30fbf69c1f2d9f88087df3a3e6dbde
Ben Young <Ben.Young@erg.com>
2022-04-07 15:55:50 -0400

    move inventory generation to new action

---
+++ tests/generate_inventories.py
@@ -0,0 +1,22 @@
+"""Create and store inventories."""
+
+import stewi
+from stewi.globals import config
+
+
+requires_browser_download = {'RCRAInfo'}
+SKIP_BROWSER_DOWNLOAD = True
+
+
+def generate_inventories(year):
+    for inventory in config()['databases']:
+        if SKIP_BROWSER_DOWNLOAD and inventory in requires_browser_download:
+            continue
+        if inventory.isin(['DMR']):
+            continue
+        df = stewi.getInventory(inventory, year)
+
+
+if __name__ == "__main__":
+    year = 2017
+    generate_inventories(year)
