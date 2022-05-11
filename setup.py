6522426468dff7a4c5d432704240ea3759a4bb8b
Ben Young <Ben.Young@erg.com>
2022-05-11 11:11:17 -0400

    :bookmark: v1.0.5 release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires=['esupy @ git+https://github.com/USEPA/esupy#egg=esupy',
 
 setup(
     name="StEWI",
-    version="1.0.4",
+    version="1.0.5",
     author="Ben Young, Wesley Ingwersen, Matthew Bergmann, Jose Hernandez-Betancur, Tapajyoti Ghosh, Eric Bell",
     author_email="ingwersen.wesley@epa.gov",
     description="Standardized Emission And Waste Inventories (StEWI)"
