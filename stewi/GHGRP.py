2e8d8a74f47d02f0984370df5120e0e4d021dbb4
Ben Young <Ben.Young@erg.com>
2022-05-11 10:59:53 -0400

    drop unnamed columns prior to fixing column labels

--- stewi/GHGRP.py
+++ stewi/GHGRP.py
@@ -245,14 +245,13 @@ def import_or_download_table(filepath, table, year, m):
     if table_df is None:
         return None
 
-    # for all columns in the temporary dataframe, remove subpart-specific prefixes
-    table_df.columns = table_df.columns.str.extract(f'.*{table}\.(.*)', expand=False)
-
     # drop any unnamed columns
-    if ('unnamed' in table_df.columns[len(table_df.columns) - 1].lower()
-        ) or (table_df.columns[len(table_df.columns) - 1] == ''):
-        table_df.drop(table_df.columns[len(table_df.columns) - 1],
-                      axis=1, inplace=True)
+    table_df = table_df.drop(columns=table_df.columns[
+            table_df.columns.str.contains('unnamed', case=False)])
+
+    # for all columns, remove subpart-specific prefixes
+    table_df.columns = table_df.columns.str.extract(f'.*{table}\.(.*)',
+                                                    expand=False)
 
     return table_df
 
